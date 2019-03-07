4c6b550747971aaa03ce76c2286352cfe7ad1d75
chencheng92 <1032699372@qq.com>
2019-03-07 12:57:04 +0800

    Initial commit

---
+++ src/com/test/TestMain.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * @program: demo001
+ * @description: 测试
+ * @author: Chencheng
+ * @create: 2019-03-07 12:55
+ **/
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("123123");
+    }
+}
